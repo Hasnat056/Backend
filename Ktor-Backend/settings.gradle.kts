79b1c7add18e6411064e2ad3a3202c26145f4516
Hasnat <rhays056@gmail.com>
2025-03-12 22:27:51 +0500

    New repo

---
+++ Ktor-Backend/settings.gradle.kts
@@ -0,0 +1,32 @@
+pluginManagement {
+    repositories {
+        // Keep Google repo for Android Studio
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        // Keep Google repo for Android Studio
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyLMS1.1"
+
+// Conditionally include modules
+if (!System.getenv().containsKey("DEPLOYING_ON_RAILWAY")) {
+    // Android Studio will see both modules
+    include(":app")
+}
+include(":Ktor-Backend")
